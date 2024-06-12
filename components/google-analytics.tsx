cb6ee2c39591f82a894ab45c35645a5ea3dd03fc
lxj <1176891456@qq.com>
2024-06-12 17:44:56 +0800

    fix:google analytics

--- components/google-analytics.tsx
+++ components/google-analytics.tsx
@@ -4,7 +4,7 @@ export const GTM_ID = process.env.NEXT_PUBLIC_GOOGLE_TAG_MANAGER_ID;
 
 const GoogleAnalytics = () => {
   return (
-    process.env.NODE_ENV === 'development' &&
+    process.env.NODE_ENV === 'production' &&
     GTM_ID && <GoogleTagManager gtmId={GTM_ID} />
   );
 };
